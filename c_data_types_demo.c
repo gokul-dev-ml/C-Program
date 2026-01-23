6d8c5bb394bfc1e574fe72aca9c855147a7776ee
GOKUL M <gokulmofficial10@gmail.com>
2026-01-23 21:47:27 +0530

    Create c_data_types_demo.c

---
+++ c_data_types_demo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=22;
+    float b=22.3;
+    char c[]= " hello";
+    printf("a = %d \n",a);
+    printf("b = %.1f\n",b);
+    printf("c = %s \n" , c);
+}
+
+//output:
+a = 22 
+b = 22.3
+c =  hello 

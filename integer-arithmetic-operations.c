fe900036cd0a113cf07486b420d1dd683965be27
GOKUL M <gokulmofficial10@gmail.com>
2026-01-23 21:53:07 +0530

    Create integer-arithmetic-operations.c

---
+++ integer-arithmetic-operations.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=22;
+    printf("number = %d\n",a);
+    a=a+25;
+    printf("after adding 25 = %d\n",a);
+    a=a*2;
+    printf("after multiplied by 2 = %d\n",a);
+}
+
+//output:
+number = 22
+after adding 25 = 47
+after multiplied by 2 = 94

db7d66e0cef2c5ae87ff3655597138925567859e
GOKUL M <gokulmofficial10@gmail.com>
2026-01-23 22:32:42 +0530

    Create logical-operators-demo.c

---
+++ logical-operators-demo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a = 25, b = 1;
+    printf("a && b : %d\n", a && b);
+    printf("a || b : %d\n", a || b);
+    printf("!a: %d\n", !a);
+
+    return 0;
+}
+
+
+//output:
+a && b : 1
+a || b : 1
+!a: 0

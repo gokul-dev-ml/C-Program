357937c0c06ff91265ce2f52be76d806d9856769
GOKUL M <gokulmofficial10@gmail.com>
2026-01-24 23:39:09 +0530

    Create for_loop_if_else.c

---
+++ for_loop_if_else.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    for(i=1;i<20;i++){
+        if(i<10)
+        printf("i is less than 10\n");
+        else
+        printf("i is greter than 10\n");
+    }
+    return 0;
+}
+
+
+
+//output:
+i is less than 10
+i is less than 10
+i is less than 10
+i is less than 10
+i is less than 10
+i is less than 10
+i is less than 10
+i is less than 10
+i is less than 10
+i is greter than 10
+i is greter than 10
+i is greter than 10
+i is greter than 10
+i is greter than 10
+i is greter than 10
+i is greter than 10
+i is greter than 10
+i is greter than 10
+i is greter than 10

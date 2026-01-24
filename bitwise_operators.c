133fa99efeb9091b74f3bedc64c678b173e86be6
GOKUL M <gokulmofficial10@gmail.com>
2026-01-24 22:37:08 +0530

    Create bitwise_operators.c

---
+++ bitwise_operators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a=20 , b=7;
+    printf("a&b =%d\n",a&b);
+    printf("a|b =%d\n",a|b);
+    printf("a^b =%d\n",a^b);
+    printf("~a =%d\n",~a);
+    printf("a<<b =%d\n",a<<b);
+    printf("a>>b =%d\n",a>>b);
+    
+    return 0;
+}
+
+
+//output:
+
+a&b =4
+a|b =23
+a^b =19
+~a =-21
+a<<b =2560
+a>>b =0

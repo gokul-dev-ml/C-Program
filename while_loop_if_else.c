db01423369e864c5759ce10397b6f12be5360eab
GOKUL M <gokulmofficial10@gmail.com>
2026-01-24 23:45:25 +0530

    Create while_loop_if_else.c

---
+++ while_loop_if_else.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    while(i<=30){
+        if(i<=15)
+        printf("i is below 15 count= %d\n",i);
+        else
+        printf("i is above 15 count=%d\n",i);
+        ++i;
+    }
+    return 0;
+}
+
+
+//output:
+
+i is below 15 count= 1
+i is below 15 count= 2
+i is below 15 count= 3
+i is below 15 count= 4
+i is below 15 count= 5
+i is below 15 count= 6
+i is below 15 count= 7
+i is below 15 count= 8
+i is below 15 count= 9
+i is below 15 count= 10
+i is below 15 count= 11
+i is below 15 count= 12
+i is below 15 count= 13
+i is below 15 count= 14
+i is below 15 count= 15
+i is above 15 count=16
+i is above 15 count=17
+i is above 15 count=18
+i is above 15 count=19
+i is above 15 count=20
+i is above 15 count=21
+i is above 15 count=22
+i is above 15 count=23
+i is above 15 count=24
+i is above 15 count=25
+i is above 15 count=26
+i is above 15 count=27
+i is above 15 count=28
+i is above 15 count=29
+i is above 15 count=30
